fe61a0d7cd0d3c262e04480a01f3f11b10f64eaa
DrFab1 <fabian.erlenbusch@freenet.de>
2020-12-16 16:18:11 +0100

    fixed error i hope somesing array

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from models import Patient, HealthValue, HealthParameter, patient_health
+from models import HealthValue, HealthParameter, Patient, PatientHealth
 from models import session, base, db
 
 db_string = "postgresql://postgres:1234@localhost:5432/patientdb"
@@ -6,8 +6,22 @@ db_string = "postgresql://postgres:1234@localhost:5432/patientdb"
 
 base.metadata.create_all(db)
 
-hv = HealthValue(parameter=1,value=20,patient=1)
+hp = HealthParameter(denotation="Hamidismus")
+
+session.add(hp)
+session.commit()
+
+hv = HealthValue(value=30, parameter=1)
 
 session.add(hv)
 session.commit()
 
+p = Patient(height=12, weight=1, first_name="Has", last_name="Mid", sex="gay")
+
+session.add(p)
+session.commit()
+
+ph = PatientHealth(patientnr=1, healthvalue=1)
+
+session.add(ph)
+session.commit()
--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer, Boolean, Float, DateTime, Table, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base  
 from sqlalchemy.orm import sessionmaker, relationship, backref
-from datetime import datetime,timezone
+from datetime import datetime, timezone
 
 
 db_string = "postgresql://postgres:1234@localhost:5432/patientdb"
@@ -14,37 +14,39 @@ session = Session()
 base.metadata.create_all(db)
 
 
-#many to many relationship for patient -> healthvalues
+# many to many relationship for patient -> healthvalues
+class PatientHealth(base):
+    __tablename__ = 'PatientHealth'
 
-patient_health = Table('patienthealth',base.metadata,Column("patient_id",Integer, ForeignKey("Patient.patient_nr")), Column("healthvalue_id",Integer,ForeignKey("healthvalue.gid")))
+    patientnr = Column(Integer, ForeignKey("Patient.patient_nr"), primary_key=True)
+    healthvalue = Column(Integer, ForeignKey("HealthValue.gid"), primary_key=True)
 
-class Patient(base):  
+
+class Patient(base):
     __tablename__ = 'Patient'
+
     height = Column(Float, nullable=False)
     weight = Column(Float, nullable=False)
     first_name = Column(String(256))
     last_name = Column(String(256), nullable=False)
     patient_nr = Column(Integer, primary_key=True)
     sex = Column(String, nullable=False)
-    healthvalue = relationship(
-        "HealthValue",
-        secondary=patient_health,
-        back_populates="patient")
+
 
 class HealthValue(base):
-    __tablename__ = 'healthvalue'
-    gid = Column(Integer,primary_key=True)
+    __tablename__ = 'HealthValue'
+
+    gid = Column(Integer, primary_key=True)
     value = Column(Integer, nullable=False)
-    parameter = Column(Integer, ForeignKey('healthparameter.hpid'))
-    patient = relationship(
-        "Patient",
-        secondary=patient_health,
-        back_populates="healthvalue")
-    #TODO: one to many relationship 
+    parameter = Column(Integer, ForeignKey('HealthParameter.hpid'))
 
 
 class HealthParameter(base):
-    __tablename__ = 'healthparameter'
+    __tablename__ = 'HealthParameter'
+
     hpid = Column(Integer, primary_key=True)
-    denotation = Column(String(256),nullable=False)
-    values = relationship('HealthValue', backref='healthparameter', lazy='dynamic')
+    denotation = Column(String(256), nullable=False)
+    values = relationship(
+        'HealthValue',
+        backref='HealthParameter',
+        lazy='dynamic')
