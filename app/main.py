aeb0cb2f42d6614a9e9e3345fd39e5daaebe76ed
mxrdck <max.rudeck@gmx.de>
2020-12-16 17:48:50 +0100

    many to many relationship fix with additional table

--- app/main.py
+++ app/main.py
@@ -1,27 +1,15 @@
-from models import HealthValue, HealthParameter, Patient, PatientHealth
+from models import HealthValue, HealthParameter, Patient, patienthealth
 from models import session, base, db
 
-db_string = "postgresql://postgres:1234@localhost:5432/patientdb"
 
 
-base.metadata.create_all(db)
 
-hp = HealthParameter(denotation="Hamidismus")
+p = session.query(Patient).first()
 
-session.add(hp)
-session.commit()
+print(p)
 
-hv = HealthValue(value=30, parameter=1)
+hv = session.query(HealthValue).first()
 
-session.add(hv)
-session.commit()
+p.patient_health.append(hv)
 
-p = Patient(height=12, weight=1, first_name="Has", last_name="Mid", sex="gay")
-
-session.add(p)
-session.commit()
-
-ph = PatientHealth(patientnr=1, healthvalue=1)
-
-session.add(ph)
 session.commit()
--- app/models.py
+++ app/models.py
@@ -1,37 +1,40 @@
-from sqlalchemy import create_engine  
+from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer, Boolean, Float, DateTime, Table, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, backref
-from datetime import datetime, timezone
+from datetime import datetime
 
 
 db_string = "postgresql://postgres:1234@localhost:5432/patientdb"
 
-db = create_engine(db_string)  
+db = create_engine(db_string)
 base = declarative_base()
-Session = sessionmaker(db)  
-session = Session()
-base.metadata.create_all(db)
-
 
 # many to many relationship for patient -> healthvalues
-class PatientHealth(base):
-    __tablename__ = 'PatientHealth'
 
-    patientnr = Column(Integer, ForeignKey("Patient.patient_nr"), primary_key=True)
-    healthvalue = Column(Integer, ForeignKey("HealthValue.gid"), primary_key=True)
+patienthealth = Table("patient_health",
+                      base.metadata,
+                      Column('patient_id', Integer,
+                             ForeignKey('Patient.patient_nr')),
+                      Column('healthvalue_id', Integer,
+                             ForeignKey('HealthValue.gid')),
+                      Column('date', DateTime, default=datetime.utcnow))
 
 
 class Patient(base):
     __tablename__ = 'Patient'
 
+    patient_nr = Column(Integer, primary_key=True)
     height = Column(Float, nullable=False)
     weight = Column(Float, nullable=False)
     first_name = Column(String(256))
     last_name = Column(String(256), nullable=False)
-    patient_nr = Column(Integer, primary_key=True)
     sex = Column(String, nullable=False)
+    patient_health = relationship('HealthValue',
+                                  secondary=patienthealth, backref='patient', lazy='dynamic')
 
+    def __repr__(self):
+        return '<Patient {}: {} {}>'.format(self.patient_nr, self.first_name, self.last_name)
 
 class HealthValue(base):
     __tablename__ = 'HealthValue'
@@ -50,3 +53,8 @@ class HealthParameter(base):
         'HealthValue',
         backref='HealthParameter',
         lazy='dynamic')
+
+
+Session = sessionmaker(db)
+session = Session()
+base.metadata.create_all(db)
