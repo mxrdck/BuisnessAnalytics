c5809e7f3201fbf4ca3764c240f2bb647d5d05c8
DrFab1 <fabian.erlenbusch@freenet.de>
2021-01-21 15:07:17 +0100

    anal

--- r_analysis/anal.R
+++ r_analysis/anal.R
@@ -82,11 +82,11 @@ sd_health_byhealth <- function(HealthParameterID) {
   print(res)
 }
 
+#TODO fix this (shows always the same)
 plot_vals_by_patient_and_health <- function(HealthParameterID, PantientID) {
   # Zeichnen Sie geeignete Diagramme zur Visualisierung der Daten mit Hilfe von ggplot
   # Alle Parameter-Werte eines bestimmten Patienten zu einem bestimmten Parameter
-  HealthParameterID <- 1
-  PantientID <- 1
+  
   sql <- paste('SELECT value 
                 FROM "Patient"
                 INNER JOIN "patient_health" ON "Patient".id="patient_health".patient_id
@@ -95,15 +95,15 @@ plot_vals_by_patient_and_health <- function(HealthParameterID, PantientID) {
                 WHERE "HealthParameter".id=', HealthParameterID, 'AND "Patient".id=', PantientID) 
   
   query <- dbGetQuery(con, sql)
-  p <- ggplot(query, aes(x=rownames(query), y=query[,])) + geom_bar(stat = "identity", width=0.2)
+  p <- ggplot(query, aes(x=rownames(query), y=query[,])) + geom_bar(stat = "identity", width=0.2) + ggtitle("Parameter Werte für Patient") + xlab("") + ylab("Wert")
   print(p)
 }
 
+#TODO fix this (shows always the same)
 plot_avg_health_by_patient <- function(PantientID) {
   # Durchschnitte der Parameterwerte über alle Patienten (also pro Patient Durchschnitt der Parameterwerte eines bestimmten Parameters)
   # peo patient durschnitt für werte
   
-  PantientID <- 1
   sql <- paste('SELECT denotation, AVG(value) 
                 FROM "Patient"
                 INNER JOIN "patient_health" ON "Patient".id="patient_health".patient_id
@@ -114,7 +114,7 @@ plot_avg_health_by_patient <- function(PantientID) {
   query <- dbGetQuery(con, sql)
   print(query)
   query <- dbGetQuery(con, sql)
-  p <- ggplot(query, aes(x=query[,1], y=query[,2])) + geom_bar(stat = "identity", width=0.2)
+  p <- ggplot(query, aes(x=query[,1], y=query[,2])) + geom_bar(stat = "identity", width=0.2) + ggtitle("Parameter Werte für Patient") + xlab("") + ylab("Wert im Durchscnitt")
   print(p)
 }
 
