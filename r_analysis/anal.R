62b94e6b9dd2f91d5db833b85700787a5e3eb03f
mxrdck <max.rudeck@gmx.de>
2021-01-26 15:10:14 +0100

    fix plots and queries

--- r_analysis/anal.R
+++ r_analysis/anal.R
@@ -69,7 +69,6 @@ median_health <- function() {
 
 sd_health_byhealth <- function(HealthParameterID) {
 # Standardabweichung der Werte eines bestimmten Gesundheitsparameters über alle Patienten
-  HealthParameterID <- 1
   sql <- paste('SELECT denotation, value
                 FROM "Patient"
                 INNER JOIN "patient_health" ON "Patient".id="patient_health".patient_id
@@ -79,6 +78,7 @@ sd_health_byhealth <- function(HealthParameterID) {
   
   query <- dbGetQuery(con, sql)
   res = aggregate(query[, 2], list(query$denotation), sd)
+  # result includes bessel'S correction (n-1)
   print(res)
 }
 
@@ -87,7 +87,7 @@ plot_vals_by_patient_and_health <- function(HealthParameterID, PantientID) {
   # Zeichnen Sie geeignete Diagramme zur Visualisierung der Daten mit Hilfe von ggplot
   # Alle Parameter-Werte eines bestimmten Patienten zu einem bestimmten Parameter
   
-  sql <- paste('SELECT value 
+  sql <- paste('SELECT value, "HealthParameter".denotation 
                 FROM "Patient"
                 INNER JOIN "patient_health" ON "Patient".id="patient_health".patient_id
                 INNER JOIN "HealthValue" ON "patient_health".healthvalue_id="HealthValue".id
@@ -95,7 +95,7 @@ plot_vals_by_patient_and_health <- function(HealthParameterID, PantientID) {
                 WHERE "HealthParameter".id=', HealthParameterID, 'AND "Patient".id=', PantientID) 
   
   query <- dbGetQuery(con, sql)
-  p <- ggplot(query, aes(x=rownames(query), y=query[,])) + geom_bar(stat = "identity", width=0.2) + ggtitle("Parameter Werte für Patient") + xlab("") + ylab("Wert")
+  p <- ggplot(query, aes(x=rownames(query), y=query[,1])) + geom_bar(stat = "identity", width=0.2, color="blue",fill="aquamarine") + ggtitle(paste(query$denotation," Werte für Patient")) + xlab("Wert ID") + ylab("Wert")
   print(p)
 }
 
@@ -104,17 +104,16 @@ plot_avg_health_by_patient <- function(HealthParameterID) {
   # Durchschnitte der Parameterwerte über alle Patienten (also pro Patient Durchschnitt der Parameterwerte eines bestimmten Parameters)
   # peo patient durschnitt für werte
   
-  sql <- paste('SELECT patient_id, AVG(value) 
+  sql <- paste('SELECT patient_id, AVG(value), "HealthParameter".denotation
                 FROM "Patient"
                 INNER JOIN "patient_health" ON "Patient".id="patient_health".patient_id
                 INNER JOIN "HealthValue" ON "patient_health".healthvalue_id="HealthValue".id
                 INNER JOIN "HealthParameter" ON "HealthValue".parameter="HealthParameter".id
-                WHERE "HealthParameter".id=', HealthParameterID, 'Group by patient_id') 
+                WHERE "HealthParameter".id=', HealthParameterID, 'Group by patient_id, denotation') 
   
   query <- dbGetQuery(con, sql)
-  print(query)
   query <- dbGetQuery(con, sql)
-  p <- ggplot(query, aes(x=query[,1], y=query[,2])) + geom_bar(stat = "identity", width=0.2) + ggtitle("Parameter Werte für Patient") + xlab("") + ylab("Wert im Durchscnitt")
+  p <- ggplot(query, aes(x=query[,1], y=query[,2])) + geom_bar(stat = "identity", width=1, color="blue",fill="aquamarine") +  ggtitle(paste("Durchschnittliche",query$denotation,"Werte pro Patient")) + xlab("Patienten ID") + ylab("Wert im Durchscnitt")
   print(p)
 }
 
